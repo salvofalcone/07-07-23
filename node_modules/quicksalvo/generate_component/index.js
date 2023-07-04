32d90375a4352effa100b1391ad12d8075a3ecdc
Salvo Falcone <salvofalcone57@gmail.com>
2023-07-04 14:45:42 +0200

    First commit

---
+++ node_modules/quicksalvo/generate_component/index.js
@@ -0,0 +1,38 @@
+import { existsSync, mkdirSync, writeFile } from 'fs';
+import { index, component, barrel } from './component_templates.js';
+
+const [name] = process.argv.slice(2);
+if (!name) throw new Error('You must include a component name.');
+
+const componentsDir = './src/components/';
+const dir = `${componentsDir}${name}/`;
+
+// Create "Components" directory if it doesn't exist
+if (!existsSync(componentsDir)) {
+  mkdirSync(componentsDir);
+}
+
+// Throw an error if the component directory already exists
+if (existsSync(dir)) {
+  throw new Error('A component with that name already exists.');
+}
+
+// Create the component directory
+mkdirSync(dir);
+console.log(`"${name}" created`);
+
+function writeFileErrorHandler(err) {
+  if (err) throw err;
+}
+
+// component.jsx
+writeFile(`${dir}/${name}.jsx`, component(name), writeFileErrorHandler);
+console.log(`Component file "${name}.jsx" created.`);
+
+// component.scss
+writeFile(`${dir}/index.css`, index(name), writeFileErrorHandler);
+console.log(`Component file "${name}.css" created.`);
+
+// index.jsx
+writeFile(`${dir}/index.js`, barrel(name), writeFileErrorHandler);
+console.log(`Component file "index.js" created.`);
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      {/* <div className="test">Ciao</div>
+      <div className="shadowTest">guarda che shadow</div>
+      <h2 className="heading1">Ciao zio</h2> */}
+
+      <div className="Typography">
+        <h1 className="heading1">Heading 1</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
